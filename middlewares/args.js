33b8afe1c1be88f78132e6124042260ced5a3a3c
FELIPE <wafflemakr@gmail.com>
2020-05-26 16:52:31 -0500

    fix middleware

--- middlewares/args.js
+++ middlewares/args.js
@@ -1,24 +1,26 @@
 const commandArgs = () => (ctx, next) => {
   if (ctx.updateType === "message") {
-    const text = ctx.update.message.text.toLowerCase();
-    if (text.startsWith("/")) {
-      const match = text.match(/^\/([^\s]+)\s?(.+)?/);
-      let args = [];
-      let command;
-      if (match !== null) {
-        if (match[1]) {
-          command = match[1];
+    if (ctx.update.message.text) {
+      const text = ctx.update.message.text.toLowerCase();
+      if (text.startsWith("/")) {
+        const match = text.match(/^\/([^\s]+)\s?(.+)?/);
+        let args = [];
+        let command;
+        if (match !== null) {
+          if (match[1]) {
+            command = match[1];
+          }
+          if (match[2]) {
+            args = match[2].split(" ");
+          }
         }
-        if (match[2]) {
-          args = match[2].split(" ");
-        }
-      }
 
-      ctx.state.command = {
-        raw: text,
-        command,
-        args,
-      };
+        ctx.state.command = {
+          raw: text,
+          command,
+          args,
+        };
+      }
     }
   }
   return next();
